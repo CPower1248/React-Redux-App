41d9ce2f498c11c56bc10849cddd2b81631dda20
corey-power <CPower1248@gmail.com>
2020-11-04 15:18:45 -0500

    Completed index.js\src and App.js

---
+++ dnd-class-app/src/store/components/ClassList.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function ClassList (props) {
+    return (
+        <div className="classlist-container" >
+            <h3>Class List</h3>
+        </div>
+    )
+}
+
+export default ClassList;
